bb20f3c8ea47c15783d35f8411f68083935f2c82
tobiaswrt <tschreiber3002@gmail.com>
2025-03-13 13:54:33 +0100

    Konfiguration der Ordnerstruktur

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return home
+def startseite():
+    return render_template('startseite.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
